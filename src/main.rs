448c13f40370524bf43cf0953ba88e4b4c804eff
jordankalebpage <jpage628@gmail.com>
2024-09-19 18:51:48 -0400

    optimizations; measure time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use chrono::prelude::*;
 use csv::ReaderBuilder;
-use std::{error::Error, fs::File, io::Write};
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufWriter, Write},
+    time::Instant,
+};
 
 #[derive(Debug, serde::Deserialize)]
 struct Record {
@@ -10,54 +15,67 @@ struct Record {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let start = Instant::now();
+
+    let read_start = Instant::now();
     let rate_records = read_sca_rates()?;
+    let read_duration = read_start.elapsed();
+
+    let write_start = Instant::now();
     write_sql_file(&rate_records)?;
+    let write_duration = write_start.elapsed();
+
+    let total_duration = start.elapsed();
+
+    println!("Read CSV time: {:?}", read_duration);
+    println!("Write SQL time: {:?}", write_duration);
+    println!("Total execution time: {:?}", total_duration);
 
     println!("All done :) またねー！");
     Ok(())
 }
 
 fn read_sca_rates() -> Result<Vec<Record>, Box<dyn Error>> {
-    let mut rate_records = Vec::new();
+    // CSV is 366 rows right now - doubt it gets much bigger. so just allocate 500 for now
+    let mut rate_records = Vec::with_capacity(500);
     let mut rdr = ReaderBuilder::new()
         .delimiter(b'|')
         .from_reader(File::open("2025_SCA_Rates.csv")?);
 
     for result in rdr.deserialize() {
-        let record = result?;
+        let record: Record = result?;
         rate_records.push(record);
     }
 
     Ok(rate_records)
 }
 
-fn write_sql_file(rate_records: &Vec<Record>) -> Result<(), Box<dyn Error>> {
+fn write_sql_file(rate_records: &[Record]) -> Result<(), Box<dyn Error>> {
     let current_fiscal_year = Local::now().year() + 1;
-    let mut sql_file = File::create_new(format!("{}_SCA_Rates.sql", current_fiscal_year))?;
+    let sql_file = File::create(format!("{}_SCA_Rates.sql", current_fiscal_year))?;
+    let mut writer = BufWriter::new(sql_file);
 
-    writeln!(sql_file, "BEGIN TRANSACTION;\n")?;
+    writeln!(writer, "BEGIN TRANSACTION;\n")?;
 
     for record in rate_records {
         writeln!(
-            sql_file,
-            "IF NOT EXISTS (SELECT 1 FROM SCA_RATES WHERE OCCUPATION_CODE = '{}') THEN\n\
+            writer,
+            "IF NOT EXISTS (SELECT 1 FROM SCA_RATES WHERE OCCUPATION_CODE = '{code}') THEN\n\
             \tINSERT INTO SCA_RATES (OCCUPATION_CODE, TITLE, RATE)\n\
-            \tVALUES ('{}', '{}', {});\n\
+            \tVALUES ('{code}', '{title}', {rate});\n\
             ELSE\n\
             \tUPDATE SCA_RATES\n\
-            \tSET RATE = {}\n\
-            \tWHERE OCCUPATION_CODE = '{}';\n\
+            \tSET RATE = {rate}\n\
+            \tWHERE OCCUPATION_CODE = '{code}';\n\
             END IF;\n",
-            record.occupation_code,
-            record.occupation_code,
-            record.title,
-            record.rate,
-            record.rate,
-            record.occupation_code
+            code = record.occupation_code,
+            title = record.title,
+            rate = record.rate
         )?;
     }
 
-    writeln!(sql_file, "COMMIT;")?;
+    writeln!(writer, "COMMIT;")?;
+    writer.flush()?;
 
     println!("SQL file created successfully!");
     Ok(())
