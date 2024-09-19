6f6ef7f35d0df1fc6ed57631f043c57c84d3814e
jordankalebpage <jpage628@gmail.com>
2024-09-19 16:17:59 -0400

    able to print records

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use std::fs::File;
+use csv::ReaderBuilder;
+use std::{error::Error, fs::File, process};
 
 #[derive(Debug, serde::Deserialize)]
 struct Record {
-    ocupation_code: String,
+    occupation_code: String,
     title: String,
-    footnote: String,
     rate: f64,
 }
 
-fn main() {}
+fn read_sca_rates() -> Result<(), Box<dyn Error>> {
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b'|')
+        .from_reader(File::open("2025_SCA_Rates.csv")?);
+
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+
+    println!("All done :) またねー！");
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = read_sca_rates() {
+        println!("error: {}", err);
+        process::exit(1);
+    }
+}
