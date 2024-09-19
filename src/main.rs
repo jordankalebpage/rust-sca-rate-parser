7ce458e52f0a6fccbd0458495202ce5eb452d1f4
jordankalebpage <jpage628@gmail.com>
2024-09-19 15:39:34 -0400

    add serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+
+#[derive(Debug, serde::Deserialize)]
+struct Record {
+    ocupation_code: String,
+    title: String,
+    footnote: String,
+    rate: f64,
 }
+
+fn main() {}
