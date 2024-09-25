ee8d7b354f438765d4ce72db0f253b03d0adb894
jordankalebpage <jpage628@gmail.com>
2024-09-25 13:03:20 -0400

    v2

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::{
 #[derive(Debug, serde::Deserialize)]
 struct Record {
     occupation_code: String,
+    description: String,
     title: String,
     rate: f64,
 }
@@ -17,9 +18,13 @@ struct Record {
 fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
 
-    let read_start = Instant::now();
-    let rate_records = read_sca_rates()?;
-    let read_duration = read_start.elapsed();
+    let read_new_start = Instant::now();
+    let mut rate_records = read_sca_rates()?;
+    let read_new_duration = read_new_start.elapsed();
+
+    let read_prev_start = Instant::now();
+    rate_records = read_previous_rates_for_descriptions(rate_records)?;
+    let read_prev_duration = read_prev_start.elapsed();
 
     let write_start = Instant::now();
     write_sql_file(&rate_records)?;
@@ -27,7 +32,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let total_duration = start.elapsed();
 
-    println!("Read CSV time: {:?}", read_duration);
+    println!("Read New SCA Rates CSV time: {:?}", read_new_duration);
+    println!("Read Previous SCA Rates CSV time: {:?}", read_prev_duration);
     println!("Write SQL time: {:?}", write_duration);
     println!("Total execution time: {:?}", total_duration);
 
@@ -50,31 +56,51 @@ fn read_sca_rates() -> Result<Vec<Record>, Box<dyn Error>> {
     Ok(rate_records)
 }
 
-fn write_sql_file(rate_records: &[Record]) -> Result<(), Box<dyn Error>> {
-    let current_fiscal_year = Local::now().year() + 1;
-    let sql_file = File::create(format!("{}_SCA_Rates.sql", current_fiscal_year))?;
-    let mut writer = BufWriter::new(sql_file);
+fn read_previous_rates_for_descriptions(
+    mut rate_records: Vec<Record>,
+) -> Result<Vec<Record>, Box<dyn Error>> {
+    let mut rdr = ReaderBuilder::new().from_reader(File::open("2023_sca_rates_export_arrs.csv")?);
 
-    writeln!(writer, "BEGIN TRANSACTION;\n")?;
-
-    for record in rate_records {
-        writeln!(
-            writer,
-            "IF NOT EXISTS (SELECT 1 FROM SCA_RATES WHERE OCCUPATION_CODE = '{code}') THEN\n\
-            \tINSERT INTO SCA_RATES (OCCUPATION_CODE, TITLE, RATE)\n\
-            \tVALUES ('{code}', '{title}', {rate});\n\
-            ELSE\n\
-            \tUPDATE SCA_RATES\n\
-            \tSET RATE = {rate}\n\
-            \tWHERE OCCUPATION_CODE = '{code}';\n\
-            END IF;\n",
-            code = record.occupation_code,
-            title = record.title,
-            rate = record.rate
-        )?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        for rate_record in &mut rate_records {
+            if rate_record.occupation_code == record.occupation_code {
+                rate_record.description = record.description;
+                break;
+            }
+        }
     }
 
-    writeln!(writer, "COMMIT;")?;
+    Ok(rate_records)
+}
+
+fn write_sql_file(rate_records: &[Record]) -> Result<(), Box<dyn Error>> {
+    let current_fiscal_year = Local::now().year() + 1;
+    let sql_file = File::create(format!(
+        "V1.1.114__Insert_{}_SCA_Rates.sql",
+        current_fiscal_year
+    ))?;
+    let mut writer = BufWriter::new(sql_file);
+
+    writeln!(
+        writer,
+        "IF NOT EXISTS (SELECT 1 FROM Jobs WHERE YEAR = {year})\n\
+        BEGIN\n",
+        year = current_fiscal_year
+    )?;
+
+    for record in rate_records {
+        writeln!(writer,
+        "INSERT INTO dbo.Jobs (JobGuid, JobCode, JobTitle, Description, HourlyWageRate, IsSCAJob, CreatedDate, Year)\n\
+        VALUES (NEWID(), '{code}', '{title}', '{description}', {rate}, 1, GETUTCDATE(), {year})\n",
+        code = record.occupation_code,
+        title = record.title,
+        description = record.description,
+        rate = record.rate,
+        year = current_fiscal_year)?;
+    }
+
+    writeln!(writer, "END;")?;
     writer.flush()?;
 
     println!("SQL file created successfully!");
